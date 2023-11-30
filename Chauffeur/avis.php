cf0af4c64648931385cc2bc23db853e2afd581c9
GravityFalls25 <thibautwindal@hotmail.com>
2023-11-30 22:20:21 +0100

    Chauffeur 30 11

---
+++ Chauffeur/avis.php
@@ -0,0 +1,77 @@
+<?php
+include_once 'navbar.php';
+
+include_once '../classes/chauffeur.php';
+include_once '../classes/course.php';
+
+
+$chauffeur = new chauffeur();
+$course = new course();
+
+
+function Afficher($liste){
+    foreach ($liste as $auto) {
+        if(empty($auto)){
+            break;
+        }
+     
+        else{
+            $ligne = '<tr>';
+        }
+        $ligne .= ' <th>' . $auto["Id"] . '</th>
+            <td>' . $auto["DateReservation"] . '</td>
+            <td>' . $auto["Nom"] . '</td>
+            <td>' . $auto["Prenom"] . '</td>
+            <td>' . $auto["IdAdresseDepart"] . '</td>
+            <td>' . $auto["IdAdresseFin"] . '</td>
+        </tr>';
+        echo $ligne;
+    }
+}
+?>
+
+<html>
+<div class="container">
+    <div class="text-center">
+        <p class="display-4 pt-4 pb-2 bold">Liste de vos avis sur vos courses</p>
+    </div>
+        
+    <div class="row align-items-center">
+    
+        <form class="col-md-4 form-group" action="" method="">
+            <input type="search" class="form-control rounded" name="search" placeholder="rechercher" aria-label="Rechercher" aria-describedby="inputGroup-sizing-sm"/>
+        </form>
+    </div>
+    <table class="table table-striped table-responsive-md">
+        <thead class="table-light">
+        <tr>
+            <th scope="col h3">Id</th>
+            <th scope="col h3">DateReservation</th>
+            <th scope="col h3">Nom</th>
+            <th scope="col h3">Prenom</th>
+            <th scope="col h3">IdAdresseDepart</th>
+            <th scope="col h3">IdAdresseFin</th>
+            <th scope="col-md-1 h3"></th>
+
+        </tr>
+        </thead>
+        <tbody class="table-group-divider">
+
+            <?php
+            
+            if(empty($_GET['search']) && empty($_GET['maitenance'])) {
+                $all = $chauffeur->Getavis($_SESSION['Id'], $Id);
+                Afficher($all);
+            }
+            else{
+                if(!empty($_GET['search'])){
+                    $resultat = $chauffeur->Rechercher($_GET['search']);
+                    Afficher($resultat);
+                }
+
+            }
+            ?>
+        </tbody>
+    </table>
+</div>
+</html>
--- classes/chauffeur.php
+++ classes/chauffeur.php
@@ -16,8 +16,7 @@ public $Mdp;
 public $NumeroDeTelephone;
 public $IdStatus;
 public $Bdd;
-private $NomTable = "personne";
-private $NomTableType = "typepersonne";
+
 
 public function __construct(){
     $db = new Bdd();
@@ -34,7 +33,54 @@ public function __construct(){
     $resultats = $req->fetchAll(PDO::FETCH_ASSOC);
     return $resultats;
     }
+    
+    public function Getcourseold($Id){
+        $req = $this->Bdd->prepare("
+    SELECT DISTINCT course.*, personne.Nom, personne.Prenom
+    FROM  course, personne
+    WHERE course.IdChauffeur =  :Id 
+    AND personne.Id = course.IdClient
+    AND course.DateReservation < CURRENT_DATE
+    
+    "); 
+    $req->bindParam(':Id', $Id);
+    $req->execute();
+    $resultats = $req->fetchAll(PDO::FETCH_ASSOC);
+    return $resultats;
+    
+}
+    public function Getcoursefutur($Id){
+        $req = $this->Bdd->prepare("
+    SELECT DISTINCT course.*, personne.Nom, personne.Prenom
+    FROM  course, personne
+    WHERE course.IdChauffeur =  :Id 
+    AND personne.Id = course.IdClient
+    AND course.DateReservation > CURRENT_DATE
 
+    "); 
+    $req->bindParam(':Id', $Id);
+    $req->execute();
+    $resultats = $req->fetchAll(PDO::FETCH_ASSOC);
+    return $resultats;
+
+}
+    public function Getavis($IdChauffeur, $Idcourse){
+     
+        $req = $this->Bdd->prepare("
+        SELECT DISTINCT course.*, personne.Nom, personne.Prenom, avis.note, avis.description
+        FROM course, personne, avis
+        WHERE course.IdChauffeur = :Id 
+        AND personne.Id = course.IdClient
+        AND avis.IdCourse = course.Id_course
+        AND course.Id = :Id_course
+");
+    $req->bindParam(':Id', $IdChauffeur);
+    $req->bindParam(':Id_course', $Idcourse);
+    $req->execute();
+    $resultats = $req->fetchAll(PDO::FETCH_ASSOC);
+    return $resultats;
+
+}
 
 }
 
