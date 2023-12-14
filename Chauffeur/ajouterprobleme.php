6e7cee4daa9be55d740991cb177dca9b4070300e
GravityFalls25 <thibautwindal@hotmail.com>
2023-12-14 22:04:57 +0100

    Visuel ajoutphotoprobleme

--- Chauffeur/ajouterprobleme.php
+++ Chauffeur/ajouterprobleme.php
@@ -1,3 +1,12 @@
+<?php
+include_once 'navbar.php';
+
+include_once '../classes/chauffeur.php';
+
+
+
+$chauffeur = new chauffeur();
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -8,30 +17,29 @@
 <body>
 
 <?php
-// Vérifier si le formulaire a été soumis
-//phpinfo();
+
 
 $uploadMaxSize = ini_get('upload_max_filesize');
 echo "La taille maximale autorisée pour les fichiers téléchargés est : $uploadMaxSize";
 
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
-    // Vérifier si un fichier a été téléchargé
+  
     if (isset($_FILES["photo"])) {
-        $uploadDir = "uploads/"; // Répertoire où vous souhaitez enregistrer les photos
+        $uploadDir = "uploads/"; 
 
-        // Vérifier si le répertoire existe, sinon le créer
+        
         if (!file_exists($uploadDir)) {
             mkdir($uploadDir, 0777, true);
         }
 
-        // Vérifier s'il y a des erreurs lors du téléchargement
+       
         if ($_FILES["photo"]["error"] === UPLOAD_ERR_OK) {
             $uploadFile = $uploadDir . basename($_FILES["photo"]["name"]);
 
-            // Vérifier si le fichier est une image réelle
+            
             $check = getimagesize($_FILES["photo"]["tmp_name"]);
             if ($check !== false) {
-                // Déplacer le fichier téléchargé vers le répertoire d'upload
+               
                 if (move_uploaded_file($_FILES["photo"]["tmp_name"], $uploadFile)) {
                     echo "La photo a été téléchargée avec succès.";
                 } else {
@@ -49,7 +57,7 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
 }
 ?>
 
-<!-- Formulaire HTML pour télécharger une photo -->
+
 <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post" enctype="multipart/form-data">
     <label for="photo">Sélectionner une photo :</label>
     <input type="file" name="photo" id="photo" accept="image/*">
